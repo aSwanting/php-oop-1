a52ba427c2bc89cb24b527c8bdfdcac1bf6b01a2
aSwanting <gab.damico@gmail.com>
2023-11-28 19:12:22 +0100

    add: cards

--- index.php
+++ index.php
@@ -6,12 +6,14 @@ class Production
     public $lang;
     public $rating;
     public $recommended;
+    public static $poster = "https://picsum.photos/200";
 
     function __construct(string $title, string $lang, int $rating)
     {
         $this->title = $title;
         $this->lang = $lang;
         $this->rating = $rating;
+        $this->set_recommended();
     }
 
     function set_recommended()
@@ -29,16 +31,59 @@ class Production
     }
 }
 
-$prod01 = new Production("The Ark", "french", 8);
-$prod01->set_recommended();
+$productions = [
+    $prod01 = new Production("The Ark", "French", 8),
+    $prod02 = new Production("Jesus Wept", "German", 3),
+    $prod03 = new Production("Rambo 17", "Spanish", 10),
+    $prod04 = new Production("Confused Gentlemen", "English", 5),
+];
 
-$prod02 = new Production("Jesus Wept", "german", 3);
-$prod02->set_recommended();
+// var_dump($productions);
 
-$prod03 = new Production("Rambo 17", "spanish", 10);
-$prod03->set_recommended();
+?>
 
-$prod04 = new Production("Confused Gentlment", "british", 5);
-$prod04->set_recommended();
 
-var_dump($prod01, $prod02, $prod03, $prod04);
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <div id="app">
+
+        <div class="container my-5">
+            <div class="row">
+
+                <?php foreach ($productions as $prod) { ?>
+
+                    <div class="col mb-3">
+                        <div class="card shadow" style="width: 18rem;">
+
+                            <img class="card-img-top" src="<?= $prod::$poster ?>">
+
+                            <div class="card-body">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item"><span class="fw-bold">Title: </span><?= $prod->title ?></li>
+                                    <li class="list-group-item"><span class="fw-bold">Language: </span><?= $prod->lang ?></li>
+                                    <li class="list-group-item"><span class="fw-bold">Rating: </span><?= $prod->rating ?></li>
+                                    <li class="list-group-item"><span class="fw-bold">Recommendation: </span><?= $prod->recommended ?></li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+
+                <?php } ?>
+
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
