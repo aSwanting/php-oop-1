a8662f5de2d9920ba505e8fb60cee26c90380614
aSwanting <gab.damico@gmail.com>
2023-11-29 16:03:03 +0100

    add: getters/setters in Production

--- Models/Production.php
+++ Models/Production.php
@@ -2,32 +2,72 @@
 
 class Production
 {
-    public $title;
-    public $lang;
-    public $rating;
-    public $recommended;
-    public $poster;
-    public $id;
+    private $title;
+    private $lang;
+    private $rating;
+    private $recommended;
 
-    function __construct(string $_title, string $_lang, float $_rating, int $_id)
+    function __construct(string $_title, string $_lang, float $_rating)
     {
-        $this->title = $_title;
-        $this->lang = $_lang;
-        $this->rating = $_rating;
-        $this->set_recommended($_rating);
-        $this->id = $_id;
-        $this->set_poster($_id);
+        $this->set_title($_title);
+        $this->set_lang($_lang);
+        $this->set_rating($_rating);
+        $this->set_recommended();
     }
 
-    public function set_recommended($_rating)
+    public function set_title($_title)
     {
-        if ($_rating <= 3) {
+        if (trim($_title)) {
+            $this->title = $_title;
+        } else {
+            $this->title = "Invalid Title";
+        }
+    }
+
+    public function get_title()
+    {
+        return $this->title;
+    }
+
+    public function set_lang($_lang)
+    {
+        if (trim($_lang)) {
+            $this->lang = $_lang;
+        } else {
+            $this->lang = "Invalid Language";
+        }
+    }
+
+    public function get_lang()
+    {
+        return $this->lang;
+    }
+
+    public function set_rating($_rating)
+    {
+        if (is_numeric($_rating) && $_rating >= 0 && $_rating <= 10) {
+            $this->rating = $_rating;
+        } else {
+            $this->rating = null;
+        }
+    }
+
+    public function get_rating()
+    {
+        return $this->rating;
+    }
+
+    public function set_recommended()
+    {
+        if (!$this->rating) {
+            $this->recommended = "Nobody Knows";
+        } else if ($this->rating <= 3) {
             $this->recommended = "Don't waste your time.";
-        } else if ($_rating <= 6) {
+        } else if ($this->rating <= 6) {
             $this->recommended = "Aggressively average.";
-        } else if ($_rating <= 8) {
+        } else if ($this->rating <= 8) {
             $this->recommended = "Highly recommend.";
-        } else if ($_rating <= 10) {
+        } else if ($this->rating <= 10) {
             $this->recommended = "Not to be missed!";
         }
     }
@@ -35,15 +75,5 @@ class Production
     public function get_recommended()
     {
         return $this->recommended;
-    }
-
-    public function set_poster($_id)
-    {
-        $this->poster = "https://picsum.photos/600?random=" . $_id;
-    }
-
-    public function get_poster()
-    {
-        return $this->poster;
     }
 }
--- index.php
+++ index.php
@@ -4,17 +4,17 @@ require_once __DIR__ . "/Models/Movie.php";
 require_once __DIR__ . "/Models/Series.php";
 
 $productions = [
-    $movie01 = new Movie("movie01", "lang", 8, 0),
-    $movie02 = new Movie("movie02", "lang", 2, 0),
-    $movie03 = new Movie("movie03", "lang", 5, 0),
-    $movie04 = new Movie("movie04", "lang", 1, 0),
-    $movie05 = new Movie("movie05", "lang", 10, 0),
+    $movie01 = new Movie("movie01", "lang", 8),
+    $movie02 = new Movie("movie02", "lang", 2),
+    $movie03 = new Movie("movie03", "lang", 5),
+    $movie04 = new Movie("movie04", "lang", 1),
+    $movie05 = new Movie("movie05", "lang", 10),
 
-    $series01 = new Series("series01", "lang", 8, 0),
-    $series02 = new Series("series02", "lang", 8, 0),
-    $series03 = new Series("series03", "lang", 8, 0),
-    $series04 = new Series("series04", "lang", 8, 0),
-    $series05 = new Series("series05", "lang", 8, 0),
+    $series01 = new Series("series01", "lang", 2),
+    $series02 = new Series("series02", "lang", 4),
+    $series03 = new Series("series03", "lang", 16),
+    $series04 = new Series("series04", "lang", 1),
+    $series05 = new Series("series05", "lang", 9),
 ];
 
 var_dump($productions);
