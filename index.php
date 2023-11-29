42bd2b2efab75e2c4df5faf8b010d2171fda9db5
aSwanting <gab.damico@gmail.com>
2023-11-29 17:18:06 +0100

    add: production cards

--- index.php
+++ index.php
@@ -17,4 +17,49 @@ $productions = [
     $series05 = new Series("Sacred Games", "Hindi", 8.6, 2),
 ];
 
-var_dump($productions);
+// var_dump($productions);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Production Listings</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+    <main class="app-main py-5">
+        <div class="container">
+            <div class="grid-wrapper">
+
+                <?php foreach ($productions as $prod) { ?>
+
+                    <div class="card grid-item shadow">
+                        <div class="card-header">
+                            <h2 class="fs-4 text-center fw-semibold"> <?= $prod->get_title() ?> </h2>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"> <span class="fw-medium">Language: </span><?= $prod->get_lang() ?> </li>
+                                <li class="list-group-item"> <span class="fw-medium">Rating: </span><?= $prod->get_rating() ?> / 10</li>
+                                <li class="list-group-item"> <span class="fw-medium">Recommendation: </span><?= $prod->get_recommended() ?> </li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                <?php } ?>
+
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
